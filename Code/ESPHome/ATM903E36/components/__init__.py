7cb35d90bf06ecb7ad5e207f0ee7a9513d893dd4
DitroniXDev <colin@turnerpost.co.uk>
2025-12-17 12:06:55 +0000

    Update __init__.py

--- Code/ESPHome/ATM903E36/components/__init__.py
+++ Code/ESPHome/ATM903E36/components/__init__.py
@@ -1,1 +1,8 @@
+import esphome.codegen as cg
 
+CODEOWNERS = ["@DitroniXDev"]
+
+atm90e36_ns = cg.esphome_ns.namespace("atm90e36")
+ATM90E36Component = atm90e36_ns.class_("ATM90E36Component", cg.Component)
+
+CONF_ATM90E36_ID = "atm90e36_id"
